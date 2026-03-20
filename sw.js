8da20adcc298019305113ad501baa8beb2f55524
Faqbj <facuuschubert@gmail.com>
2026-03-20 11:10:38 -0300

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,39 @@
+var CACHE_NAME = 'gymlog-v2';
+var URLS_TO_CACHE = ['./gymlog.html', './manifest.json', './icon-512.png'];
+
+self.addEventListener('install', function(event) {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(function(cache) {
+      return cache.addAll(URLS_TO_CACHE);
+    })
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', function(event) {
+  event.waitUntil(
+    caches.keys().then(function(names) {
+      return Promise.all(
+        names.filter(function(n) { return n !== CACHE_NAME; })
+             .map(function(n) { return caches.delete(n); })
+      );
+    })
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', function(event) {
+  event.respondWith(
+    fetch(event.request).then(function(response) {
+      // Network first: always try fresh, cache the result
+      var clone = response.clone();
+      caches.open(CACHE_NAME).then(function(cache) {
+        cache.put(event.request, clone);
+      });
+      return response;
+    }).catch(function() {
+      // Offline fallback: serve from cache
+      return caches.match(event.request);
+    })
+  );
+});
